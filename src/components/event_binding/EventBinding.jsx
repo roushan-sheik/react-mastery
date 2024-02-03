58c41f431f1b930351182a702333198199a63bb3
roushan-sheik <roushansheik@gmail.com>
2024-02-03 19:14:07 +0600

    Event binding added with another way

--- src/components/event_binding/EventBinding.jsx
+++ src/components/event_binding/EventBinding.jsx
@@ -6,22 +6,22 @@ class EventBinding extends Component {
     this.state = {
       count: 0,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  //NOTE -  Increment method
+  handleClick() {
+    console.log(this);
+    this.setState({
+      count: this.state.count + 1,
+    });
   }
   render() {
-    //NOTE -  Increment
-    function handleClick() {
-      console.log(this);
-      this.setState({
-        count: this.state.count + 1,
-      });
-    }
-
     const { count } = this.state;
     return (
       <div className="container m-auto">
         <h1>Counter State change App</h1>
         <h2>{count}</h2>
-        <button onClick={handleClick.bind(this)} className="btn  btn-primary">
+        <button onClick={this.handleClick} className="btn  btn-primary">
           Increment
         </button>
       </div>
