38e839bbe682b231453e631fa69216fd79dc4356
roushan-sheik <roushansheik@gmail.com>
2024-02-22 10:04:50 +0600

    active nav bar added

--- src/components/react-router-dom/components/Nav.jsx
+++ src/components/react-router-dom/components/Nav.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Nav = () => {
   return (
     <nav>
       <div className="navbar bg-base-100 w-full flex gap-8">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/blogs"}>Blogs</Link>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        <NavLink to={"/blogs"}>Blogs</NavLink>
       </div>
     </nav>
   );
